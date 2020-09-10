a737bb98bc881fff5afbb10bb6de8a8a333866d3
solankijaykishan91 <70826265+solankijaykishan91@users.noreply.github.com>
2020-09-10 17:10:41 +0530

    LetsUpgrad
    Assignment2.3_day3_python 
    Prime numbers between 1,200

---
+++ Assingment2.3_day3_jaykishan.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# print prime numbers between till 
+for j in range(1,201): 
+    j=int(input("enter number between:"))
+    if j>1:
+        for i in range(2,j):
+            if (j%i)==0:
+                print(j,"is not prime number")
+                break
+            else:
+                print(j,"is prime number")
+                break
+              
+         
